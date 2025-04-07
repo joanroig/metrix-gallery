5e7cbc7f0ac70f30d09b33e0f74b784bca83498c
Joan <12451419+joanroig@users.noreply.github.com>
2025-04-07 23:41:54 +0200

    feat: add the other 10k gifs

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:metrix_gallery/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const GifGalleryApp());
+    await tester.pumpWidget(const GifGalleryApp(initialThemeMode: ThemeMode.light, initialContrastLevel: 2));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
